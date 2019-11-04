fe133ec8a4ad7ee96ca520129b02b8578c7b5afe
kasperosterbye <kasper.osterbye@gmail.com>
2019-11-04 07:11:33 +0100

    no splitting version

--- src/main/java/cphbusiness/ufo/co2tablereader/CO2ReaderMain.java
+++ src/main/java/cphbusiness/ufo/co2tablereader/CO2ReaderMain.java
@@ -13,22 +13,35 @@ public class CO2ReaderMain {
 
     // The next file is a local copy of: https://datahub.io/core/co2-fossil-by-nation/r/fossil-fuel-co2-emissions-by-nation.csv
     private static final String INFILE = "fossil-fuel-co2-emissions-by-nation_csv.csv";
+    //390944446
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        for (int repeat = 0; repeat < 100; repeat++) {
+        final int N = 1000;
+        for (int repeat = 1; repeat <= N; repeat++) {
             try (BufferedReader br = new BufferedReader(new FileReader(INFILE))) {
                 String line = br.readLine();
-                if (repeat == 0) System.out.println(line);
+                if (repeat == N) System.out.println(line);
                 line = br.readLine();
                 long sum = 0;
                 while (line != null) {
-                    //for (int i = 0; i<10;i++){
                     String[] elements = line.split(",");
                     sum += Integer.parseInt(elements[2]);
                     line = br.readLine();
                 }
-                if (repeat==0) System.out.println("Total: " + sum);
+                if (repeat==N) System.out.println("Total: " + sum);
             }
         }
+    }
+    public static int getTotal(String line){
+        int index =0;
+        while(line.charAt(index)!=',') index++; // find first ,
+        index++;
+        while(line.charAt(index)!=',') index++; // find seccond ,
+        index++;
+        int total = 0; char c;
+        while((c = line.charAt(index))!=',') {
+            total = total*10+(c-'0');
+            index++;}
+        return total;
     }
 }
